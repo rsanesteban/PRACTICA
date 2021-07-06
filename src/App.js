353191ac6c4b2c76b9976f89a479419a751977c0
Roberto San Esteban Santillana <58464906+rsanesteban@users.noreply.github.com>
2021-07-06 12:57:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           Su prima la coja.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
