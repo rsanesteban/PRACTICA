e82476d1e47ec4371760e973591198edb259ccab
Roberto San Esteban Santillana <58464906+rsanesteban@users.noreply.github.com>
2021-07-06 13:16:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function Welcome(props) {
 function App() {
   return (
     <div>
-      <Welcome name="Sara" />      <Welcome name="Cahal" />      <Welcome name="Edite" />    </div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />    </div>
   );
 }
 
