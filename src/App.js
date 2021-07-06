d4c357dc45861947a46ac99893905bde05721882
Roberto San Esteban Santillana <58464906+rsanesteban@users.noreply.github.com>
2021-07-06 13:19:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,2 @@
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
-
-function App() {
-  return (
-    <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />    </div>
-  );
-}
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import logo from './logo.svg';
+import './App.css';
