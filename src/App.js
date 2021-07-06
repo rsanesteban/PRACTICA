a0d363056e7ad8e0026d7a5b9e6d852f8b630a25
Roberto San Esteban Santillana <58464906+rsanesteban@users.noreply.github.com>
2021-07-06 12:52:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          La <code>madre que pario</code> and save to reload.
+          Su prima la coja.
         </p>
         <a
           className="App-link"
